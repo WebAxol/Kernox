d7643d8aad913380fcf351a21648bfa116ea9a10
WebAxol <webaxol@gmail.com>
2025-06-24 11:05:22 -0600

    Fixed namespace resolution at class 'CollectionManager'.

--- lib/collection/CollectionManager.d.ts
+++ lib/collection/CollectionManager.d.ts
@@ -1,6 +1,6 @@
-import { AbstractCollection } from "./AbstractCollection";
-import { Entity } from "../entity/Entity";
-import { Kernox } from "../Kernox";
+import { AbstractCollection } from "./AbstractCollection.js";
+import { Entity } from "../entity/Entity.js";
+import { Kernox } from "../Kernox.js";
 export declare class CollectionManager {
     private __kernox;
     private collections;
